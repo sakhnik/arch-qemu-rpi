232fc0aacf8b38af25d1097836968b98d3b19825
Anatolii Sakhnik <sakhnik@gmail.com>
2018-06-30 22:47:02 +0300

    Update arch-qemu-rpi.sh to use arch-chroot

--- arch-qemu-rpi.sh
+++ arch-qemu-rpi.sh
@@ -3,45 +3,21 @@
 # The original idea is taken from:
 # http://kbeckmann.github.io/2017/05/26/QEMU-instead-of-cross-compiling/
 #
-# Assuming qemu-user-static and binfmt-support have already been installed.
-# Also, binfmt support has been enabled:
-#   sudo update-binfmts --enable
+# Assuming qemu-user-static has already been installed.
 
-[[ -d alarm ]] || {
+target=alarm
+
+[[ -d $target ]] || {
     wget -c -O /tmp/alarm.tar.gz http://os.archlinuxarm.org/os/ArchLinuxARM-rpi-2-latest.tar.gz
-    mkdir -p alarm
-    bsdtar -xpf /tmp/alarm.tar.gz -C alarm
+    mkdir -p $target
+    bsdtar -xpf /tmp/alarm.tar.gz -C $target
 }
 
 # Ensure binfmt support is enabled
 pacman -Qi binfmt-qemu-static >/dev/null 2>&1
 
-cd alarm
-
-function cleanup
-{
-    rm -rf /tmp/alarm.tar.gz
-    sudo umount ./proc
-    sudo umount ./dev
-    sudo umount ./tmp
+diff -q $target/usr/bin/qemu-arm-static /usr/bin/qemu-arm-static || {
+    sudo cp /usr/bin/qemu-arm-static $target/usr/bin/
 }
 
-trap cleanup EXIT
-
-diff -q ./usr/bin/qemu-arm-static /usr/bin/qemu-arm-static || {
-    sudo cp /usr/bin/qemu-arm-static ./usr/bin/
-}
-
-# Mount the /proc file system
-sudo mount -t proc proc ./proc
-# Hack: Replace ./etc/mtab with a copy of your mounts
-unlink ./etc/mtab
-cat /proc/self/mounts > ./etc/mtab
-# Hack: Hard code a nameserver in ./etc/resolv.conf since systemd isn't running
-unlink ./etc/resolv.conf
-echo "nameserver 8.8.8.8" > ./etc/resolv.conf
-# Sometimes it's nice to have /dev/null. If needed, mount it in:
-sudo mount -o bind /dev ./dev
-sudo mount -o bind /tmp ./tmp
-
-sudo chroot . ./bin/bash
+sudo arch-chroot $target /bin/bash
